58deebc0fa992072f0247fc8c737e34e90502043
roguefaction <vytas.k@live.com>
2020-04-11 17:33:20 +0300

    initial commit

---
+++ src/main/java/lt/vytauto_kondrato_ukis/pasatrija_web/PasatrijaWebApplication.java
@@ -0,0 +1,13 @@
+package lt.vytauto_kondrato_ukis.pasatrija_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasatrijaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasatrijaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/vytauto_kondrato_ukis/pasatrija_web/PasatrijaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.vytauto_kondrato_ukis.pasatrija_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasatrijaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
